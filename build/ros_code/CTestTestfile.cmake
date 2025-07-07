bd6d2406287ba6c5cab8f0b7f2841327f17236e5
Guo-xiaohu924 <guofenfen657@gmail.com>
2025-07-07 16:48:15 +0800

    6 submit

---
+++ build/ros_code/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robot/tutorial_ws/src/ros_code
+# Build directory: /home/robot/tutorial_ws/build/ros_code
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
